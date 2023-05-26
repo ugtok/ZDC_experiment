9f390eb1cc113bb19d2e318dbc71075f12ec2733
ufuk <ufuk.guney.tok@cern.ch>
2023-05-26 10:14:19 +0200

    Update

--- CSVtoRoot.py
+++ CSVtoRoot.py
@@ -7,6 +7,10 @@ Description: Dark current of the PMTs from ZDC's RPD detectors.
 Script usage: "python CSVtoRoot.py --region"
         e.g   python CSVtoRoot.py Plus
 '''
+import ROOT
+import sys
+import os
+
 #fileNames_Minus=['230524_A1_Ch1.csv']
 fileNames_Minus=["ZDC_Minus/ZDCM_waveform/230524_A1_Ch1.csv","ZDC_Minus/ZDCM_waveform/230524_A1_Ch2.csv","ZDC_Minus/ZDCM_waveform/230524_A1_Ch3.csv","ZDC_Minus/ZDCM_waveform/230524_A1_Ch4.csv","ZDC_Minus/ZDCM_waveform/230524_A2_Ch5.csv","ZDC_Minus/ZDCM_waveform/230524_A2_Ch6.csv","ZDC_Minus/ZDCM_waveform/230524_A2_Ch7.csv","ZDC_Minus/ZDCM_waveform/230524_A2_Ch8.csv","ZDC_Minus/ZDCM_waveform/230524_B3_Ch9.csv","ZDC_Minus/ZDCM_waveform/230524_B3_Ch10.csv","ZDC_Minus/ZDCM_waveform/230524_B3_Ch11.csv","ZDC_Minus/ZDCM_waveform/230524_B3_Ch12.csv","ZDC_Minus/ZDCM_waveform/230524_B4_Ch13.csv","ZDC_Minus/ZDCM_waveform/230524_B4_Ch14.csv","ZDC_Minus/ZDCM_waveform/230524_B4_Ch15.csv","ZDC_Minus/ZDCM_waveform/230524_B4_Ch16.csv"]
 
@@ -37,9 +41,9 @@ for fileName in fileNames:
         rootfileName = "ZDC_%s/RPD_%s_rootFiles/RPD_%s_Channel_%s.root"%(region,region,region,count)
         treeName = "Channel_%s"%str(count)
 
-        histo1D = df.Histo1D(("dark current","RPD %s, Channel_%s; Time;Voltage"%(region,count),150,-0.001,0.001),"Voltage")
+        histo1D = df.Histo1D(("Channel_%s"%count,"RPD %s; Time (ns);Voltage (mV)"%region,150,-0.0002,0.001),"Voltage")
 
-        histo1D.Fit("gaus")
+	histo1D.Fit("gaus","V","",0.0,0.0008)
         df.Snapshot(treeName,rootfileName,"",snapshotOptions)
         f = ROOT.TFile.Open(rootfileName)
         c = ROOT.TCanvas()
